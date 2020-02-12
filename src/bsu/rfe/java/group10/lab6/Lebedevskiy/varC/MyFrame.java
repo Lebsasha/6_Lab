c612da6c45f1d27379296c015c913d56efd5e35f
Alexander <Lebedevskiy_s@rambler.ru>
2020-02-13 00:07:38 +0300

    Translate to the English and remove comments

--- src/bsu/rfe/java/group10/lab6/Lebedevskiy/varC/MyFrame.java
+++ src/bsu/rfe/java/group10/lab6/Lebedevskiy/varC/MyFrame.java
@@ -11,44 +11,35 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 
 public class MyFrame extends JFrame {
-    // Константы, задающие размер окна приложения, если оно
-// не распахнуто на весь экран
     private static final int WIDTH = 700;
     private static final int HEIGHT = 500;
     private JMenuItem pauseMenuItem;
     private JMenuItem resumeMenuItem;
-    // Поле, по которому прыгают мячи
     private Field field = new Field();
-    // Конструктор главного окна приложения
     public MyFrame() {
-        super("Программирование и синхронизация потоков");
+        super("Balls");
         setSize(WIDTH, HEIGHT);
         Toolkit kit = Toolkit.getDefaultToolkit();
-// Отцентрировать окно приложения на экране
         setLocation((kit.getScreenSize().width - WIDTH)/2,
                 (kit.getScreenSize().height - HEIGHT)/2);
-// Установить начальное состояние окна развѐрнутым на весь экран
         setExtendedState(MAXIMIZED_BOTH);
-// Создать меню
         JMenuBar menuBar = new JMenuBar();
         setJMenuBar(menuBar);
-        JMenu ballMenu = new JMenu("Мячи");
-        Action addBallAction = new AbstractAction("Добавить мяч") {
+        JMenu ballMenu = new JMenu("Balls");
+        Action addBallAction = new AbstractAction("Add ball") {
             public void actionPerformed(ActionEvent event) {
                 field.addBall();
                 if (!pauseMenuItem.isEnabled() &&
                         !resumeMenuItem.isEnabled()) {
-// Ни один из пунктов меню не являются
-// доступными - сделать доступным "Паузу"
                     pauseMenuItem.setEnabled(true);
                 }
             }
         };
         menuBar.add(ballMenu);
         ballMenu.add(addBallAction);
-        JMenu controlMenu = new JMenu("Управление");
+        JMenu controlMenu = new JMenu("Control");
         menuBar.add(controlMenu);
-        Action pauseAction = new AbstractAction("Приостановить движение"){
+        Action pauseAction = new AbstractAction("Pause"){
             public void actionPerformed(ActionEvent event) {
                 field.pause();
                 pauseMenuItem.setEnabled(false);
@@ -57,7 +48,7 @@ public class MyFrame extends JFrame {
         };
         pauseMenuItem = controlMenu.add(pauseAction);
         pauseMenuItem.setEnabled(false);
-        Action resumeAction = new AbstractAction("Возобновить движение") {
+        Action resumeAction = new AbstractAction("Continue") {
             public void actionPerformed(ActionEvent event) {
                 field.resume();
                 pauseMenuItem.setEnabled(true);
@@ -66,12 +57,9 @@ public class MyFrame extends JFrame {
         };
         resumeMenuItem = controlMenu.add(resumeAction);
         resumeMenuItem.setEnabled(false);
-// Добавить в центр граничной компоновки поле Field
         getContentPane().add(field, BorderLayout.CENTER);
     }
-    // Главный метод приложения
     public static void main(String[] args) {
-// Создать и сделать видимым главное окно приложения
         MyFrame frame = new MyFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
